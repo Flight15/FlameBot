8ca9e859550afe4f6935b9a8532fa2fa5e455891
Flight15 <omrigez9@gmail.com>
2020-04-07 12:00:21 +0300

    nothing....

--- discordbot.py
+++ discordbot.py
@@ -21,6 +21,11 @@ async def ping(ctx):
 
 
 @client.command()
+async def liberate(ctx):
+    await ctx.send("THE REVOLUTION BEGINS NOW!")
+    await ctx.send("!play https://www.youtube.com/watch?v=X3wBsZfHRy4")
+
+@client.command()
 async def rank(ctx, name):
     if name == "gross":
         await ctx.send("you dont wanna know...")
