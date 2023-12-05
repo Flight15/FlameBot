df2e90cc6a16954def74dd27adc1a39eff50f3a9
OmriGez <omri@getport.io>
2023-12-05 19:10:19 +0200

    added decrypt

---
+++ .github/workflows/decrypt.py
@@ -0,0 +1,28 @@
+import base64
+import json
+import os
+from Crypto.Cipher import AES
+
+PORT_CLIENT_SECRET = os.getenv("PORT_CLIENT_SECRET")
+# ENCRYPTED_PROPERTY = os.getenv("ENCRYPTED_PROPERTY")
+ENCRYPTED_PROPERTY="yCkpNW4e5wAHTmNsXwHfAR8n3ERrarCwRAq+I5OhygVpIMy/x/llssDs+qpUeg=="
+
+PROPERY_IS_JSON = True
+
+key = PORT_CLIENT_SECRET[:32].encode()
+
+
+encrypted_property_value = base64.b64decode(ENCRYPTED_PROPERTY)
+
+iv = encrypted_property_value[:16]
+ciphertext = encrypted_property_value[16:-16]
+mac = encrypted_property_value[-16:]
+
+cipher = AES.new(key, AES.MODE_GCM, iv)
+
+# decrypt the property
+decrypted_property_value = cipher.decrypt_and_verify(ciphertext, mac)
+property_value = json.loads(decrypted_property_value) if PROPERY_IS_JSON else decrypted_property_value
+
+
+print(property_value)
